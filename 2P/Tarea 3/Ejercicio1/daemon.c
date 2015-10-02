2b5495aa75d40f4f2994ba14fe46d6245f20d917
Alejandra Garay <alejandragaray@MacBook-Pro-de-Alejandra-2.local>
2015-10-02 15:24:28 -0500

    Tarea 3

--- 2P/Tarea 3/Ejercicio1/daemon.c
+++ 2P/Tarea 3/Ejercicio1/daemon.c
@@ -9,53 +9,47 @@
 #include <string.h>
 
 int main(void) {
+    
+    pid_t pid, sid;
+    pid = fork();
+    
+    if (pid < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+    
+    if (pid > 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    
+    umask(0);
+    sid = setsid();
+    
+    if (sid < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+    
+    if ((chdir("/")) < 0)
+    {
+        exit(EXIT_FAILURE);
+    }
+    
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+    
+    char *command = "command";
+    char *arguments[] = { "command", "ps -A"};
+    
+    
+    while (1)
+    {
         
-        /* Our process ID and Session ID */
-        pid_t pid, sid;
+        execvp(command, arguments);
         
-        /* Fork off the parent process */
-        pid = fork();
-        if (pid < 0) {
-                exit(EXIT_FAILURE);
-        }
-        /* If we got a good PID, then
-           we can exit the parent process. */
-        if (pid > 0) {
-                exit(EXIT_SUCCESS);
-        }
-
-        /* Change the file mode mask */
-        umask(0);
-                
-        /* Open any logs here */        
-                
-        /* Create a new SID for the child process */
-        sid = setsid();
-        if (sid < 0) {
-                /* Log the failure */
-                exit(EXIT_FAILURE);
-        }
-        
-
-        
-        /* Change the current working directory */
-        if ((chdir("/")) < 0) {
-                /* Log the failure */
-                exit(EXIT_FAILURE);
-        }
-        
-        /* Close out the standard file descriptors */
-        close(STDIN_FILENO);
-        close(STDOUT_FILENO);
-        close(STDERR_FILENO);
-        
-        /* Daemon-specific initialization goes here */
-        
-        /* The Big Loop */
-        while (1) {
-           /* Do some task here ... */
-           
-           sleep(30); /* wait 30 seconds */
-        }
-   exit(EXIT_SUCCESS);
+        sleep(10);
+    }
+    exit(EXIT_SUCCESS);
 }
