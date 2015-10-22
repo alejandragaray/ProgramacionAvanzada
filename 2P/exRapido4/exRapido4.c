d4df6d3a07661a48a21055fcd697b8a70f169f9f
Alejandra Garay <alejandragaray51@gmail.com>
2015-10-22 15:11:05 -0500

    ex4

--- 2P/exRapido4/exRapido4.c
+++ 2P/exRapido4/exRapido4.c
@@ -1,19 +1,74 @@
-#include <dirent.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <signal.h>
 #include <stdio.h>
-#include <sys/stat.h>
+#include <stdlib.h>
+#include <pthread.h>
 
 
+pthread_mutex_t mutex_1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_2 = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutex_3 = PTHREAD_MUTEX_INITIALIZER;
 
-int main()
+int * elementos = malloc(3 * sizeof(int));
+
+int fumado;
+
+void* fumador1(void* arg)
 {
-	
-    }
-
-	return 0;
+    
 }
 
+void* fumador2(void* arg)
+{
+    pthread_mutex_lock(&mutex_s);
+    sleep(5);
+    fumado = 0;
+	   
+    pthread_mutex_unlock(&mutex_s);
+    
+    pthread_exit(0);
+}
+
+void* fumador3(void* arg)
+{
+    pthread_mutex_lock(&mutex_s);
+    sleep(5);
+    fumado = 0;
+	   
+    pthread_mutex_unlock(&mutex_s);
+    
+    pthread_exit(0);
+}
+
+void* agente(void* arg)
+{
+    
+    pthread_mutex_lock(&mutex_s);
+    sleep(5);
+    
+    fumado = 0;
+    
+    pthread_mutex_unlock(&mutex_s);
+    
+    pthread_exit(0);
+}
+
+
+int main(int argc, char* arvg[])
+{
+    pthread_t * tid;
+    int nhilos;
+    int i;
+    
+    nhilos = 4;
+    
+    tid = malloc(nhilos * sizeof(pthread_t));
+    
+    printf("Creando hilos ...\n");
+    
+    pthread_create(tid, NULL, agente, (void *)0);
+    pthread_create(tid+1, NULL, fumador1, (void *)1);
+    pthread_create(tid+2, NULL, fumador2, (void *)2);
+    pthread_create(tid+3, NULL, fumador3, (void *)3);
+    
+    
+    return 0;
+}
