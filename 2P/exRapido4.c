6f9a36145383d39d769787a4a90a366638102ed0
Alejandra Garay <alejandragaray51@gmail.com>
2015-10-22 14:26:50 -0500

    er4

---
+++ 2P/exRapido4.c
@@ -0,0 +1,19 @@
+#include <dirent.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/stat.h>
+
+
+
+int main()
+{
+	
+    }
+
+	return 0;
+}
+
