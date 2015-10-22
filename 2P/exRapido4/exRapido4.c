b289a4d94718ce6cbd104762a0feb18591ed32bb
Alejandra Garay <alejandragaray51@gmail.com>
2015-10-22 15:21:50 -0500

    ex4

--- 2P/exRapido4/exRapido4.c
+++ 2P/exRapido4/exRapido4.c
@@ -1,39 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <time.h>
 
 
 pthread_mutex_t mutex_1 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_2 = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t mutex_3 = PTHREAD_MUTEX_INITIALIZER;
 
-int * elementos = malloc(3 * sizeof(int));
+int elementos[3] = {1,2,3};
 
-int fumado;
+//1 = papel, 2 = tabaco, 3 = fosforo
+
+int elemento = 0;
 
 void* fumador1(void* arg)
 {
-    
-}
-
-void* fumador2(void* arg)
-{
-    pthread_mutex_lock(&mutex_s);
+    pthread_mutex_lock(&mutex_1);
     sleep(5);
-    fumado = 0;
-	   
-    pthread_mutex_unlock(&mutex_s);
+    
+    elemento = 1;
+    pthread_mutex_unlock(&mutex_1);
     
     pthread_exit(0);
 }
 
-void* fumador3(void* arg)
+void* fumador2(void* arg)
 {
-    pthread_mutex_lock(&mutex_s);
+    pthread_mutex_lock(&mutex_2);
     sleep(5);
-    fumado = 0;
+    elemento = 2;
 	   
-    pthread_mutex_unlock(&mutex_s);
+    pthread_mutex_unlock(&mutex_2);
+    
+    pthread_exit(0);
+}
+
+void* fumador1(void* arg)
+{
+    pthread_mutex_lock(&mutex_3);
+    sleep(5);
+    elemento = 3;
+	   
+    pthread_mutex_unlock(&mutex_3);
     
     pthread_exit(0);
 }
@@ -41,12 +50,13 @@ void* fumador3(void* arg)
 void* agente(void* arg)
 {
     
-    pthread_mutex_lock(&mutex_s);
-    sleep(5);
+    while (pthread_mutex_trylock(&mutex_1))
+    {
+        pthread_mutex_unlock(&mutex_1);
+        
+        pthread_mutex_lock(&mutex_1);
+    }
     
-    fumado = 0;
-    
-    pthread_mutex_unlock(&mutex_s);
     
     pthread_exit(0);
 }
@@ -54,6 +64,7 @@ void* agente(void* arg)
 
 int main(int argc, char* arvg[])
 {
+    srand(time(NULL));
     pthread_t * tid;
     int nhilos;
     int i;
@@ -69,6 +80,11 @@ int main(int argc, char* arvg[])
     pthread_create(tid+2, NULL, fumador2, (void *)2);
     pthread_create(tid+3, NULL, fumador3, (void *)3);
     
+    for (i = 0; i < nhilos; ++i) {
+        pthread_join(*(tid+i), NULL);
+        printf("TID = %d...\n", *(tid+i));
+    }
     
+    free(tid);
     return 0;
 }
