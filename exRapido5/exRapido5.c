08a0d14e92af54f9152dc46c994ae71637c6d85f
Alejandra Garay <alejandragaray51@gmail.com>
2015-10-29 14:40:05 -0600

    ER5

---
+++ exRapido5/exRapido5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+
+pthread_mutex_t mutex_1 = PTHREAD_MUTEX_INITIALIZER;
+
+int main(int argc, char* arvg[])
+{
+
+    return 0;
+}
